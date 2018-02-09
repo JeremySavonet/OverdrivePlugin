de9a3dcdc87e7b1bcb8972755d10d8df66214bd0
jeremySavonet <jeremy.savonet@gmail.com>
2018-02-09 13:54:25 +0100

    Add Builds folder

---
+++ Builds/CLion/cmake-build-debug/CMakeFiles/AU.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Overrdrive.component/Contents/Resources/Overrdrive.rsrc"
+  "CMakeFiles/AU.dir/Users/savonetj/Documents/PERSO/OverdrivePlugin/JuceLibraryCode/include_juce_audio_plugin_client_AU_1.mm.o"
+  "CMakeFiles/AU.dir/Users/savonetj/Documents/PERSO/OverdrivePlugin/JuceLibraryCode/include_juce_audio_plugin_client_AU_2.mm.o"
+  "Overrdrive.component/Contents/MacOS/Overrdrive.pdb"
+  "Overrdrive.component/Contents/MacOS/Overrdrive"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
